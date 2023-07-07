2af6c2642c2829b01c54f18224ffa557ec767d5e
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-07 16:08:24 -0600

    put prompt into prompt.js

--- index.js
+++ index.js
@@ -1,51 +1,8 @@
 const fs = require("fs");
-const inquirer = require(`inquirer`);
 const { Triangle, Square, Circle } = require("./lib/shapes.js");
+const { prompName, promptNameColor, promptShape, promptShapeColor } = require("./lib/prompt.js");
 
 
-function prompName() {
-    return inquirer.prompt([
-        {
-            name: 'name',
-            message: 'Please enter the name of your logo (3 characters or less):',
-            validate: function (input) {
-                if (input.length <= 3) {
-                    return true;
-                }
-                return 'You can only enter 3 or less characters';
-            },
-        },
-    ]);
-}
-
-function promptNameColor() {
-    return inquirer.prompt([
-        {
-            name: 'nameColor',
-            message: 'Enter the color code for the logo name( you can use keyword or hexadecimal number):',
-        },
-    ]);
-}
-
-function promptShape() {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Please choose a follow shape:',
-            choices: ['Circle', 'Triangle', 'Square'],
-        },
-    ]);
-}
-
-function promptShapeColor() {
-    return inquirer.prompt([
-        {
-            name: 'shapeColor',
-            message: 'Enter the shape color (keyword or hexadecimal number):',
-        },
-    ]);
-}
 
 function generateLogo(name, nameColor, shape, shapeColor) {
     let logoShape;
